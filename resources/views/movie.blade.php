846c4369dc83dc2ce84abd5bda5390b589414d58
Dmitriy <dima.vasilenko.com@gmail.com>
2022-01-28 12:45:39 +0200

    Part#1: single movie and archive movie page

---
+++ resources/views/movie.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row py-5">
+
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img width="230px" src="https://image.tmdb.org/t/p/w500/">
+                    </div>
+                    <div class="col-md-8">
+                        <h1 class="fw-bold">t</h1>
+                        <p class="blog-post-meta">
+                        <span class="fw-bold ">
+                            IMDb:
+                        </span>
+                            $movie imdb
+                        </p>
+                        <p class="blog-post-meta">
+                        <span class="fw-bold">
+                            Kinopoisk:
+                        </span>
+                            $movie kinopoisk
+                        </p>
+                        <p class="blog-post-meta">
+                        <span class="fw-bold">
+                            Time:
+                        </span>
+                            $movie time
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h2 class="pt-2">Description:</h2>
+                    c
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
